0df795121cee1679a4fa270024c96b588326140a
mikealllowercase <mikealllowercase@gmail.com>
2017-05-15 13:14:31 -0400

    Update Relativity.java

--- src/Relativity.java
+++ src/Relativity.java
@@ -3,7 +3,7 @@ public class Relativity
 	public static double gameSpeed = 1;
 	public static void main(String[] args)
 	{
-		Player p1 = new Player(50, 50, 5, 400, 3.0, 7, 1.0, 0.7, "Shadows");
+		Player p1 = new Player(50, 50, 5, 400, 3.0, 7, 1.0, 0.7, "Albert");
 		Levels.initialize();
 		
 		GameWindow gw = new GameWindow(p1);
